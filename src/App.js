e4b338f879fb5ac4552418697e86d6cd911c5c37
MeetOdedara <meedodedara123@gmail.com>
2023-02-22 15:34:00 -0500

    Navbar working for students

--- src/App.js
+++ src/App.js
@@ -7,7 +7,8 @@ import Events from "./Events/Events";
 import Navbar from "./Navbar";
 import { Route, Routes } from "react-router-dom";
 import Login from "./Login/Login";
-
+import Attendance from "./Attendance/Attendance";
+import ContactUs from "./ContactUs/ContactUs";
 function App() {
  
 
@@ -21,6 +22,10 @@ function App() {
         <Route path="/DashBoard" element={<DashBoard />} />
         <Route path="/MyBooking" element={<MyBookings />} />
         <Route path="/Events" element={<Events />} />
+        <Route path="/Attendance" element={<Attendance />} />
+        <Route path="/contact-us" element={<ContactUs />} />
+
+
       </Routes>
     </>
   );
---
+++ src/Attendance/Attendance.js
@@ -0,0 +1,6 @@
+import React from "react";
+function Attendance() {
+  return <>Attendance</>;
+}
+
+export default Attendance;
